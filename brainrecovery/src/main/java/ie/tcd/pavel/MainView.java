b07e33f15172ba639bc57fdbbd274c626268d641
Cian Jinks <cjinks99@gmail.com>
2020-06-06 00:08:09 +0100

    Created a basic MainView

--- brainrecovery/src/main/java/ie/tcd/pavel/MainView.java
+++ brainrecovery/src/main/java/ie/tcd/pavel/MainView.java
@@ -1,9 +1,12 @@
 package ie.tcd.pavel;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.charts.model.Title;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -11,18 +14,8 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * A sample Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
- * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
- */
+import java.util.ArrayList;
+
 @Route
 @PWA(name = "Vaadin Application",
         shortName = "Vaadin App",
@@ -32,34 +25,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     * @param service The message service. Automatically injected Spring managed bean.
-     */
-    public MainView(@Autowired GreetService service) {
+    public MainView() {
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+        this.setSizeFull();
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+        H1 title = new H1("Brain Recovery");
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        Button five = new Button("5 Minutes");
+        Button ten = new Button("10 Minutes");
+        Button thirty = new Button("30 Minutes");
+        Button hour = new Button("60 Minutes");
+        five.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        ten.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        thirty.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        hour.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        ArrayList<Button> buttons = new ArrayList<>();
+        buttons.add(five);
+        buttons.add(ten);
+        buttons.add(thirty);
+        buttons.add(hour);
+
+        //VerticalLayout vl = new VerticalLayout();
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        //addClassName("centered-content");
+        add(title);
+        setAlignItems(Alignment.CENTER);
+        for(Button button : buttons) {
+            button.setWidth("25em");
+            button.setHeight("10em");
+            add(button);
+        }
     }
 
 }
